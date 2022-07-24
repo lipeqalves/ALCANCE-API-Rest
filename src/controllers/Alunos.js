0a92919b7549754d4baa93464127cbd3e29b77fd
Filipe <lipeqa@gmail>
2022-07-23 23:55:00 -0300

    feat: adiciona validações para os atributos da entidade aluno

--- src/controllers/Alunos.js
+++ src/controllers/Alunos.js
@@ -1,13 +1,24 @@
 import AlunoModel from "../models/AlunoModel.js"
+import ValidacoesServices from "../services/ValidacoesServices.js"
 
 class Alunos {
     static rotas(app){
-        app.get("/alunos", (rec, res) =>{
-            const aluno = new AlunoModel(10)
+        app.get("/alunos", (req, res) =>{
+
+            const aluno = new AlunoModel(10, "oi")
             res.status(200).json(aluno)
         })
-        app.get("/alunos/:id", (rec,res) => {
-            res.status(200).send("lista alunos por id")
+
+        app.post("/alunos",(req, res) =>{
+            
+            const ehValido = ValidacoesServices.validaNome(req.body.nome)
+            
+            if(!ehValido){
+                const aluno = new AlunoModel(10, "filipe")
+                res.status(201).json(aluno)
+            }else{
+             res.status(400).send("Erro")
+            }
         })
     }
 }
---
+++ src/infra/Database.js
@@ -0,0 +1,15 @@
+export const Database = {
+
+    Alunos : [
+        {
+            id:01,
+            nome:"Filipe Alves",
+            email: "filipe@resilia.com",
+            telefone: "81996436651",
+            turma: "turma inicial",
+            id_curso: "18"
+        }
+    ],
+
+}
+

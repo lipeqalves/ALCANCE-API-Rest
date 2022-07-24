bc899be2efd6b7d2ec046e56d4287661f31bfa85
Filipe <lipeqa@gmail>
2022-07-23 22:14:54 -0300

    feat: adiciona o modelo para a entidade aluno

---
+++ src/models/AlunoModel.js
@@ -0,0 +1,12 @@
+class AlunoModel {
+    constructor(id, nome, email, telefone, turma, id_curso){
+        this.id = id,
+        this.nome = nome,
+        this.email = email,
+        this.telefone = telefone,
+        this.turma = turma,
+        this.id_curso = id_curso 
+    }
+}
+
+export default AlunoModel

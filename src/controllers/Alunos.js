50a8ad4edb25e9c25dfe0bf06f7089ffdde47392
Filipe <lipeqa@gmail>
2022-07-24 01:00:47 -0300

    faz requisição com ao banco de dados com so verbos get e post

--- src/controllers/Alunos.js
+++ src/controllers/Alunos.js
@@ -1,12 +1,14 @@
 import AlunoModel from "../models/AlunoModel.js"
 import ValidacoesServices from "../services/ValidacoesServices.js"
+import DatabaseMetodos from "../utils/DatabaseMetodos.js"
+
 
 class Alunos {
     static rotas(app){
         app.get("/alunos", (req, res) =>{
-
-            const aluno = new AlunoModel(10, "oi")
-            res.status(200).json(aluno)
+            //const aluno = new AlunoModel(10, "oi")
+            //const response = DatabaseMetodos.listarAlunos()
+            res.status(200).json(DatabaseMetodos.listarAlunos())
         })
 
         app.post("/alunos",(req, res) =>{
@@ -14,10 +16,11 @@ class Alunos {
             const ehValido = ValidacoesServices.validaNome(req.body.nome)
             
             if(!ehValido){
-                const aluno = new AlunoModel(10, "filipe")
-                res.status(201).json(aluno)
+                //const aluno = new AlunoModel(10, "filipe")
+                const aluno = new AlunoModel(...Object.values(req.body))
+                res.status(201).json(DatabaseMetodos.adicionaAluno(aluno))
             }else{
-             res.status(400).send("Erro")
+             res.status(400).json({Erro:"Erro"})
             }
         })
     }

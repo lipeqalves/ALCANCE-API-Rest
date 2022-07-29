dc90968452569e7a2730b3a80f840ad03dd27951
Juliana <julianaha01@gmail.com>
2022-07-29 10:55:06 -0300

    fix: reestrutura classe no arquivo DAO

--- src/DAO/DAO.js
+++ src/DAO/DAO.js
@@ -1,22 +1,22 @@
 import Database from "../infra/Database.js";
 
-class DAO{
-    static ativaChavesEstrangeiras(){
+class DAO {
+    static async ativaChavesEstrangeiras() {
         const query = 'PRAGMA foreign_keys = ON';
 
         Database.run(query, error => {
-            if(error){
-                console.log(error.mensage);
+            if (error) {
+                console.log(error.message);
             } else {
                 console.log("Chaves estrangeiras estão ativas");
             }
         });
     }
-    static createTable(query){
+    static createTable(query) {
 
-        return new Promise((resolve, reject)=>{
-            Database.run(query, (e)=>{
-                if(e){
+        return new Promise((resolve, reject) => {
+            Database.run(query, (e) => {
+                if (e) {
                     reject(e.message)
                 } else {
                     resolve("Tabela criada com sucesso!")
@@ -25,10 +25,24 @@ class DAO{
         })
     }
 
-    static listarTodos(query){
-        return new Promise((resolve, reject)=> {
-            Database.all(query, (e, resultado)=>{
-                if(e){
+    static inserir(entidade, query) {
+        const body = Object.values(entidade)
+
+        return new Promise((resolve, reject) => {
+            Database.run(query, [...body], (e) => {
+                if (e) {
+                    reject(e.message)
+                } else {
+                    resolve({error: false, message: "Cadastrado com sucesso!" })
+                }
+            })
+        })
+    }
+
+    static listarTodos(query) {
+        return new Promise((resolve, reject) => {
+            Database.all(query, (e, resultado) => {
+                if (e) {
                     reject(e.message)
                 } else {
                     resolve(resultado)
@@ -37,31 +51,43 @@ class DAO{
         })
     }
 
-    // static listarPorId(id, query){
-    //     return new Promise((resolve, reject)=> {
-    //         Database.get(query, id, (e, resultado)=>{
-    //             if(e){
-    //                 reject(e)
-    //             } else {
-    //                 resolve(resultado)
-    //             }
-    //         })
-    //     })
-    // }
-
-    static inserir(entidade, query){
-        const body = Object.values(entidade)
-
-        return new Promise((resolve, reject)=>{
-            Database.run(query, [...body], (e)=>{
-                if(e){
+    static listarPorId(id, query) {
+        return new Promise((resolve, reject) => {
+            Database.get(query, id, (e, resultado) => {
+                if (e) {
                     reject(e.message)
                 } else {
-                    resolve({error: false, message: "Cadastrado com sucesso!"})
+                    resolve(resultado)
+                }
+            })
+        })
+    }
+
+    static atualizaPorId(entidade, id, query) {
+        const body = Object.values(entidade)
+        return new Promise((resolve, reject) => {
+            Database.run(query, [...body, id], (e, result) => {
+                if (e) {
+                    reject(e.message)
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }
+
+    static deletaPorId(query, id) {
+        return new Promise((resolve, reject) => {
+            Database.run(query, id, (e) => {
+                if (e) {
+                    reject(e.message)
+                } else {
+                    resolve({ erro: false, message: `Registro com Id ${id} deletado com sucesso` })
                 }
             })
         })
     }
 }
+
 
 export default DAO

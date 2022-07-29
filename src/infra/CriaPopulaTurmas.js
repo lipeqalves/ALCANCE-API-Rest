13a741c40acddaa64180759688bb6176432a8c00
Filipe Alves <lipeqa@gmail.com>
2022-07-29 18:23:53 -0300

    fix : corrigi erro de rota da entidade turmas

--- src/infra/CriaPopulaTurmas.js
+++ src/infra/CriaPopulaTurmas.js
@@ -1,36 +1,32 @@
-import DatabaseTurmasMetodos from "../DAO/DataBaseTurmasMetodos";
+import DatabaseTurmasMetodos from "../DAO/DatabaseTurmasMetodos.js";
 import DAO from "../DAO/DAO.js";
 
-const turmas = [
-  {
+const turmas = [{
     classificacao: "A",
     data_inicio: "10/02/2022",
     data_formatura: "10/11/2024",
     professor: "Paulo Souza",
-    curso: "Informatica Basica",
-  },
-  {
+    curso: "Informatica Basica"
+  },{
     classificacao: "B",
     data_inicio: "10/03/2022",
     data_formatura: "10/12/2024",
     professor: "Sandra Castro",
-    curso: "Informatica Intermediario",
-  },
-  {
+    curso: "Informatica Intermediario"
+  },{
     classificacao: "C",
     data_inicio: "10/04/2022",
     data_formatura: "10/01/2025",
     professor: "Monica Santoro",
-    curso: "Informatica Avançado",
-  },
-];
+    curso: "Informatica Avançado"
+  }]
 
 try {
   await DAO.ativaChavesEstrangeiras();
   const response = await DatabaseTurmasMetodos.createTableTurmas();
   console.log(`Tabela Turmas: ${response}`);
 
-  cursos.forEach(async function (turma) {
+  turmas.forEach(async function (turma) {
     const resposta = await DatabaseTurmasMetodos.inserirTurmas(turma);
     console.log(resposta);
   });

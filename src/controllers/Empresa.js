023b4111752fe653c4106b252f7dcd5b8263838f
Roberta <roberttavalentim07@icloud.com>
2022-07-29 19:02:55 -0300

    adicionado post, put e delete em empresa controllers

--- src/controllers/Empresa.js
+++ src/controllers/Empresa.js
@@ -13,12 +13,36 @@ class Empresa {
             res.status(200).json(response)
         })
 
+        app.get("/empresa/:id", async(req, res) =>{
+            const response = await DatabaseEmpresaMetodos.listarEmpresaPorId(req.params.id)
+            res.status(200).json(response)
+        })
+
         app.post("/empresa",async (req, res) =>{
+
+            const ehValidaEmpresa = ValidacoesServices.validaEmpresa(...Object.values(req.body))
+             if(ehValidaEmpresa){
+
 
             const empresa = new EmpresaModel(...Object.values(req.body))
             const response = await DatabaseEmpresaMetodos.adicionaEmpresa(empresa)
             res.status(201).json(response)
 
+        }else{
+            res.status(400).json({Erro:"Erro"})
+           }
+
+        })
+
+        app.put("/empresa/:id", async (req, res) => {
+            const empresa = new EmpresaModel(...Object.values(req.body))
+            const response = await DatabaseEmpresaMetodos.atualizaEmpresa(...Object.values(req.params.id), empresa)
+            res.status(201).json(response)
+        })
+
+        app.delete("/empresa/:id", async (req, res) => {
+            const response = await DatabaseEmpresaMetodos.excluirEmpresa(...Object.values(req.params.id))
+            res.status(201).json(response)
         })
 
     }

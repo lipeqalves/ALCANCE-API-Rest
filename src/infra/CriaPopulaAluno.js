7107290cefb676ad315b4a0e83456715f3572ea0
Filipe Alves <lipeqa@gmail.com>
2022-07-29 18:24:43 -0300

    feat: adiciona a entidade matricula, em teste

--- src/infra/CriaPopulaAluno.js
+++ src/infra/CriaPopulaAluno.js
@@ -1,26 +1,24 @@
 import DatabaseAlunoMetodos from "../DAO/DatabaseAlunoMetodos.js";
 import DAO from "../DAO/DAO.js";
 
+const aluno = {
+    nome:"Aluno",
+    email: "aluno@test.br" ,
+    telefone: "8199999999",
+    turma:"01",
+    curso:"Informática Avançada"
+}
 
-    const aluno = {
-        nome:"Aluno",
-        email: "aluno@test.br" ,
-        telefone: "8199999999",
-        turma:"01",
-        curso:"Informática Avançada"
-    }
-
-    try {
-        await DAO.ativaChavesEstrangeiras() 
-        
-        await DatabaseAlunoMetodos.createTableAlunos()
-        
-        await DatabaseAlunoMetodos.adicionaAluno(aluno)
-        
-    }catch (e){
-        console.log(e.message)
-    }
-
+try {
+    await DAO.ativaChavesEstrangeiras() 
+    
+    await DatabaseAlunoMetodos.createTableAlunos()
+    
+    await DatabaseAlunoMetodos.adicionaAluno(aluno)
+    
+}catch (e){
+    console.log(e.message)
+}
         
         
 

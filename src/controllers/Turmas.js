c9e91687c607012d49f842a45435a9372f427e6c
CarmeLeticia <carme.leticia@gmail.com>
2022-07-29 16:19:55 -0300

    feat: adiciona rotas

--- src/controllers/Turmas.js
+++ src/controllers/Turmas.js
@@ -1,2 +1,20 @@
 import DatabaseMetodos from "../utils/DatabaseMetodos.js";
 import ValidacoesServices from "../services/ValidacoesServices.js";
+
+class Turmas {
+  static rotas(app) {
+    app.get("/turmas", async (req, res) => {
+      res.send("Funcionando");
+    });
+
+    app.get("/turmas/:id", async (req, res) => {});
+
+    app.post("/turmas", async (req, res) => {});
+
+    app.put("/turmas/:id", (req, res) => {});
+
+    app.delete("/turmas/:id", async (req, res) => {});
+  }
+}
+
+export default Cursos;

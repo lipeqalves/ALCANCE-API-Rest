1602fc14822aa905e98bd3e69aab72ad662d6a3e
CarmeLeticia <carme.leticia@gmail.com>
2022-07-29 17:22:28 -0300

    feat: adiciona rotas

--- src/controllers/Turmas.js
+++ src/controllers/Turmas.js
@@ -1,21 +1,53 @@
-import DatabaseMetodos from "../utils/DatabaseMetodos.js";
+import DatabaseTurmasMetodos from "../DAO/DatabaseTurmasMetodos.js";
 import ValidacoesServices from "../services/ValidacoesServices.js";
 import TurmasModel from "../models/TurmasModels.js";
 
 class Turmas {
   static rotas(app) {
-    app.get("/turmas", async (req, res) => {
-      res.send("Funcionando");
+    app.get("/cursos", async (req, res) => {
+      const response = await DatabaseTurmasMetodos.listarTurmas();
+      res.status(200).json(response);
     });
 
-    app.get("/turmas/:id", async (req, res) => {});
+    app.get("/turmas/:id", async (req, res) => {
+      const response = await DatabaseTurmasMetodos.listarTurmasPorId(
+        req.params.id
+      );
+      res.status(200).json(response);
+    });
 
-    app.post("/turmas", async (req, res) => {});
+    app.post("/turmas", async (req, res) => {
+      try {
+        const turma = new TurmasModel(...Object.values(req.body));
+        const response = await DatabaseTurmasMetodos.inserirTurmas(turma);
+        res.status(201).json(response);
+      } catch (error) {
+        res.status(400).json(error.message);
+      }
+    });
 
-    app.put("/turmas/:id", (req, res) => {});
+    app.put("/turmas/:id", (req, res) => {
+      const turma = new TurmasModel(...Object.values(req.body));
+      const response = DatabaseTurmasMetodos.atualizaTurmaPorId(
+        req.params.id,
+        cursos
+      );
+      res.status(201).json(response);
+      // } else {
+      res.status(400).json({ Erro: "Erro" });
+    });
 
-    app.delete("/turmas/:id", async (req, res) => {});
+    app.delete("/turmas/:id", async (req, res) => {
+      try {
+        const turma = await DatabaseTurmasMetodos.deletaTurmaPorId(
+          req.params.id
+        );
+        res.status(200).json(turmas);
+      } catch (error) {
+        res.status(404).json({ Error: error.message });
+      }
+    });
   }
 }
 
-export default Cursos;
+export default Turmas;

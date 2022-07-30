bec4cd27ad8e8c3c73fee16bbdfcf11a84c94f83
Juliana <julianaha01@gmail.com>
2022-07-30 17:05:18 -0300

    cria rotas entidade formacao

---
+++ src/controllers/Formacao.js
@@ -0,0 +1,24 @@
+class Formacao {
+    static rotas(app) {
+        app.get("/formacao", async (req, res) => {
+         res.send('rota funcionando')
+        })
+
+        app.get("/formacao/:id", async (req, res) => {
+    
+        })
+
+        app.post("/formacao", async (req, res) => {
+
+        })
+
+        app.put("/formacao/:id", (req, res) => {
+    
+        })
+
+        app.delete("/formacao/:id", async (req, res) => {
+        })
+    }
+}
+
+export default Formacao

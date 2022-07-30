884d0da01dc3bf94097d40559779ba2365528d88
Filipe Alves <lipeqa@gmail.com>
2022-07-29 21:30:07 -0300

    fix: atualiza o criaPopulaAluno usa forech

--- src/infra/CriaPopulaAluno.js
+++ src/infra/CriaPopulaAluno.js
@@ -1,25 +1,44 @@
 import DatabaseAlunoMetodos from "../DAO/DatabaseAlunoMetodos.js";
 import DAO from "../DAO/DAO.js";
 
-const aluno = {
-    nome:"Aluno",
-    email: "aluno@test.br" ,
-    telefone: "8199999999",
-    turma:"01",
-    curso:"Informática Avançada"
-}
+const alunos = [
+    {
+        nome: "Filipe",
+        email: "aluno@test.br",
+        telefone: "8199999999",
+        turma: "01",
+        curso: "Informática Avançada"
+    },
+    {
+        nome: "Juliana",
+        email: "aluno@test.br",
+        telefone: "8199999999",
+        turma: "01",
+        curso: "Programação Neurolinguistica"
+    },
+    {
+        nome: "Roberta",
+        email: "aluno@test.br",
+        telefone: "8199999999",
+        turma: "01",
+        curso: "Programação Neurolinguistica"
+    }
+]
 
 try {
-    await DAO.ativaChavesEstrangeiras() 
-    
-    await DatabaseAlunoMetodos.createTableAlunos()
-    
-    await DatabaseAlunoMetodos.adicionaAluno(aluno)
-    
-}catch (e){
+    await DAO.ativaChavesEstrangeiras()
+
+    const response = await DatabaseAlunoMetodos.createTableAlunos()
+    console.log(`Tabela Alunos: ${response}`)
+
+    alunos.forEach(async (aluno) => {
+        const response = await DatabaseAlunoMetodos.adicionaAluno(aluno)
+        console.log(response)
+    })
+} catch (e) {
     console.log(e.message)
 }
-        
-        
+
+
 
 

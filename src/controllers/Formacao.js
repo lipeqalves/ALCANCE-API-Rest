3b5126cff5306f56d4868e7e525ddf4d33f6229e
Juliana <julianaha01@gmail.com>
2022-07-30 19:56:37 -0300

    feat: adiciona database em rotas entidade formacao

--- src/controllers/Formacao.js
+++ src/controllers/Formacao.js
@@ -1,27 +1,50 @@
 import FormacaoModel from "../models/FormacaoModel.js"
-import DatabaseFormacao from "../DAO/DatabaseFormacaoMetodos.js"
+import DatabaseFormacaoMetodos from "../DAO/DatabaseFormacaoMetodos.js"
 
 class Formacao {
     static rotas(app) {
         app.get("/formacao", async (req, res) => {
-         res.send('rota funcionando')
+            const response = await DatabaseFormacaoMetodos.listarFormacao();
+            res.status(200).json(response);
         })
 
         app.get("/formacao/:id", async (req, res) => {
-    
+            const response = await DatabaseFormacaoMetodos.listarFormacaoPorId(
+                req.params.id
+            );
+            res.status(200).json(response);
+
         })
 
         app.post("/formacao", async (req, res) => {
+            try {
+                const formacao = new FormacaoModel(...Object.values(req.body));
+                const response = await DatabaseFormacaoMetodos.inserirFormacao(formacao);
+                res.status(201).json(response);
+            } catch (error) {
+                res.status(400).json(error.message);
+            }
 
         })
 
         app.put("/formacao/:id", (req, res) => {
-    
+            const formacao = new FormacaoModel(...Object.values(req.body));
+            const response = DatabaseFormacaoMetodos.atualizaFormacaoPorId(req.params.id, formacao);
+            res.status(201).json(response);
+            res.status(400).json({ Erro: "Erro" });
+
         })
 
         app.delete("/formacao/:id", async (req, res) => {
-        })
+            try {
+                const formacao = await DatabaseFormacaoMetodos.deletaFormacaoPorId(
+                    req.params.id
+                );
+                res.status(200).json(formacao);
+            } catch (error) {
+                res.status(404).json({ Error: error.message });
+            }
+        });
     }
 }
-
 export default Formacao

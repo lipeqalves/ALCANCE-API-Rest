453f1054202b8e219feed03863604666fb6ae321
Juliana <julianaha01@gmail.com>
2022-07-31 14:40:39 -0300

    test: inicia validacao em controllers entidade cursos

--- src/controllers/Cursos.js
+++ src/controllers/Cursos.js
@@ -1,5 +1,6 @@
 import DatabaseCursosMetodos from "../DAO/DatabaseCursosMetodos.js"
 import CursosModel from "../models/CursosModel.js"
+import ValidacoesCursos from "../services/ValidacoesCursos.js"
 
 class Cursos {
     static rotas(app) {
@@ -9,45 +10,50 @@ class Cursos {
         })
 
         app.get("/cursos/:id", async (req, res) => {
-            const response = await DatabaseCursosMetodos.listarCursosPorId(req.params.id)
-            res.status(200).json(response)
+            try {
+                const cursos = await DatabaseCursosMetodos.listarCursosPorId(req.params.id)
+                if (!cursos) {
+                    throw new Error("Curso não encontrado com esse Id")
+                }
+                res.status(200).json(response)
+            } catch (error) {
+                res.status(400).json(error.message)
+            }
         })
 
         app.post("/cursos", async (req, res) => {
-
-            // const isValid = ValidacoesServices.isValid(...Object.values(req.body))
-
+            const validaCursos = ValidacoesCursos.validaCurso(...Object.values(req.body))
             try {
-                // if (isValid) {
-                const curso = new CursosModel(...Object.values(req.body))
-                const response = await DatabaseCursosMetodos.inserirCursos(curso)
-                res.status(201).json(response)
-                // // } else {
-                //     throw new Error("Requisição incompleta, revise o corpo da mesma")
-                // }
+                if (validaCursos) {
+                    const curso = new CursosModel(...Object.values(req.body))
+                    const response = await DatabaseCursosMetodos.inserirCursos(curso)
+                    res.status(201).json(response)
+                } else {
+                    throw new Error("Requisição incompleta, revise o corpo da mesma")
+                }
             } catch (error) {
                 res.status(400).json(error.message)
             }
         })
 
         app.put("/cursos/:id", (req, res) => {
-            // const isValid = ValidacoesService.isValid(...Object.values(req.body))
+            const validaCursos = ValidacoesCursos.validaCurso(...Object.values(req.body))
 
-            // if(isValid){
-            const cursos = new CursosModel(...Object.values(req.body))
-            const response = DatabaseCursosMetodos.atualizaCursoPorId(req.params.id, cursos)
-            res.status(201).json(response)
-            // } else {
-            res.status(400).json({ Erro: "Erro" })
-            // }
+            if (validaCursos) {
+                const cursos = new CursosModel(...Object.values(req.body))
+                const response = DatabaseCursosMetodos.atualizaCursoPorId(req.params.id, cursos)
+                res.status(201).json(response)
+            } else {
+                res.status(400).json()
+            }
         })
 
         app.delete("/cursos/:id", async (req, res) => {
+            const cursos = await DatabaseCursosMetodos.deletaCursoPorId(req.params.id)
             try {
-                const cursos = await DatabaseCursosMetodos.deletaCursoPorId(req.params.id)
-                // if(!usuario){
-                //     throw new Error("Usuário não encontrado")
-                // }
+                if (!cursos) {
+                    throw new Error("Curso não encontrado")
+                }
                 res.status(200).json(cursos)
             } catch (error) {
                 res.status(404).json({ Error: error.message })

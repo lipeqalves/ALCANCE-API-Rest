845158c1b6f92545c51967276a77cbd5f5775b77
Filipe Alves <lipeqa@gmail.com>
2022-07-30 21:18:47 -0300

    feat: adiciona a entidade matricula com todas as funcinalidades

--- src/controllers/Matricula.js
+++ src/controllers/Matricula.js
@@ -1,12 +1,41 @@
 import MatriculaModel from "../models/MatriculaModel.js"
-import DatabeseMatriculaMetodos from "../DAO/DatabeseMatriculaMetodos.js"
+import DatabeseMatriculaMetodos from "../DAO/DatabaseMatriculaMetodos.js"
 
 class Matricula {
-    static rotas(app){
-        app.get("/matriculas", async(req, res) => {
-            const response = await DatabeseMatriculaMetodos.listarMatricula()
+    static rotas(app) {
+        app.get("/matriculas", async (req, res) => {
+            const response = await DatabeseMatriculaMetodos.listarMatriculas()
             res.status(200).json(response)
         })
+
+        app.get("/matriculas/:id", async (req, res) => {
+            const response = await DatabeseMatriculaMetodos.listarMatriculaPorId(req.params.id)
+            res.status(200).json(response)
+        })
+
+        app.post("/matriculas", async (req, res) => {
+            // const isValid = ValidacoesServices.isValid(...Object.values(req.body))
+            // if(isValid){
+
+            const matricula = new MatriculaModel(...Object.values(req.body))
+            const response = await DatabeseMatriculaMetodos.adicionaMatriculas(matricula)
+            res.status(201).json(response)
+
+            // }else{
+            //  res.status(400).json({Erro:"Erro"})
+            // }
+        })
+        app.put("/matriculas/:id", async (req, res) => {
+            const matricula = new MatriculaModel(...Object.values(req.body))
+            const response = await DatabeseMatriculaMetodos.atualizaMatricula(...Object.values(req.params.id), matricula)
+            res.status(201).json(response)
+        })
+
+        app.delete("/matriculas/:id", async (req, res) => {
+            const response = await DatabeseMatriculaMetodos.excluirMatricula(...Object.values(req.params.id))
+            res.status(201).json(response)
+        })
+
     }
 }
 
--- src/models/MatriculaModel.js
+++ src/models/MatriculaModel.js
@@ -2,7 +2,7 @@ class MatriculaModel {
     constructor(aluno,data_inicio, numero_matricula, turma, curso){
         
         this.aluno = aluno,
-        this.inicio = data_inicio,
+        this.data_inicio = data_inicio,
         this.numero_matricula = numero_matricula,
         this.turma = turma,
         this.curso = curso 

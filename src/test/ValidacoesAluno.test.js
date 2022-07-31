90aeab17ba19bdef478384fcdf8ae9e553e80383
Filipe Alves <lipeqa@gmail.com>
2022-07-30 22:43:26 -0300

    feat: adiciona test para validações de email e telefone

--- src/test/ValidacoesAluno.test.js
+++ src/test/ValidacoesAluno.test.js
@@ -1,8 +1,12 @@
 import ValidacoesService from "../services/ValidacoesServices.js";
 
 
-test("valida se o nome é maior ou igua a 3",()=>{
-    expect(ValidacoesService.validaNome("filipe")).toBe(true)
+test("valida se o nome e sobrenome é maior que 3",()=>{
+    expect(ValidacoesService.validaNome("Filipe")).toBe(true)
+})
+
+test("verifica se o nome digitado é um numero",()=>{
+    expect(ValidacoesService.validaNome("123")).toBe(false)
 })
 
 test("valida se o nome é maior ou igua a 3",()=>{
@@ -11,6 +15,26 @@ test("valida se o nome é maior ou igua a 3",()=>{
 
 test("valida se o e-mail está no padrão string@string.com",()=>{
     expect(ValidacoesService.validaEmail("filipe@teste.com")).toBe(true)
+})
+
+test("valida se o e-mail está no padrão string@string.com",()=>{
+    expect(ValidacoesService.validaEmail("filipe.com")).toBe(false)
+})
+
+test("valida se o e-mail está no padrão string@string.com",()=>{
+    expect(ValidacoesService.validaEmail("filipe@com")).toBe(false)
+})
+
+test("valida se o e-mail está no padrão string@string.com",()=>{
+    expect(ValidacoesService.validaTelefone("5511988888888")).toBe(true)
+})
+
+test("valida se o e-mail está no padrão string@string.com",()=>{
+    expect(ValidacoesService.validaTelefone("5511988888")).toBe(false)
+})
+
+test("valida se o e-mail está no padrão string@string.com",()=>{
+    expect(ValidacoesService.validaTelefone("988888")).toBe(false)
 })
 
 

d881d1a972cd70cba619f23cba6fdebc5153f78a
kensku <kensku07@gmail.com>
2022-07-31 15:58:26 -0300

    ProfessoresModel added

---
+++ src/models/ProfessoresModel.js
@@ -0,0 +1,10 @@
+class ProfessoresModel{
+    constructor(nome, email, telefone, disciplinas){
+        this.nome = nome,
+        this.email = email,
+        this.telefone = telefone,
+        this.disciplinas = disciplinas
+    }
+}
+
+export default ProfessoresModel;

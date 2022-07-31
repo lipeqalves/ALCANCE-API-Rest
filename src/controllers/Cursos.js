99de5bec0b29d677293dd558f28068f31ab5d769
Juliana <julianaha01@gmail.com>
2022-07-31 17:49:18 -0300

    fix: refatora validacao da entidade cursos

--- src/controllers/Cursos.js
+++ src/controllers/Cursos.js
@@ -11,11 +11,11 @@ class Cursos {
 
         app.get("/cursos/:id", async (req, res) => {
             try {
-                const cursos = await DatabaseCursosMetodos.listarCursosPorId(req.params.id)
-                if (!cursos) {
+                const curso = await DatabaseCursosMetodos.listarCursosPorId(req.params.id)
+                if (!curso) {
                     throw new Error("Curso não encontrado com esse Id")
                 }
-                res.status(200).json(response)
+                res.status(200).json(curso)
             } catch (error) {
                 res.status(400).json(error.message)
             }
@@ -36,27 +36,30 @@ class Cursos {
             }
         })
 
-        app.put("/cursos/:id", (req, res) => {
+        app.put("/cursos/:id", async (req, res) => {
             const validaCursos = ValidacoesCursos.validaCurso(...Object.values(req.body))
-
-            if (validaCursos) {
-                const cursos = new CursosModel(...Object.values(req.body))
-                const response = DatabaseCursosMetodos.atualizaCursoPorId(req.params.id, cursos)
-                res.status(201).json(response)
-            } else {
-                res.status(400).json()
+            try {
+                if (validaCursos) {
+                    const cursos = new CursosModel(...Object.values(req.body))
+                    const response = await DatabaseCursosMetodos.atualizaCursoPorId(req.params.id, cursos)
+                    res.status(201).json(response)
+                } else {
+                    throw new Error("Requisição inválida, revise o corpo da mesma")
+                }
+            } catch (error) {
+                res.status(400).json(error.message)
             }
         })
 
         app.delete("/cursos/:id", async (req, res) => {
-            const cursos = await DatabaseCursosMetodos.deletaCursoPorId(req.params.id)
             try {
-                if (!cursos) {
+                const curso = await DatabaseCursosMetodos.deletaCursoPorId(req.params.id)
+                if (!curso) {
                     throw new Error("Curso não encontrado")
                 }
-                res.status(200).json(cursos)
+                res.status(200).json(curso)
             } catch (error) {
-                res.status(404).json({ Error: error.message })
+                res.status(404).json(error.message)
             }
 
         })
--- src/services/ValidacoesServices.js
+++ src/services/ValidacoesServices.js
@@ -1,6 +1,6 @@
 class ValidacoesService {
   /**
-   *
+   *Metodo valida se string tem mais que 3 caracteres
    * @param {string} nome
    * @returns boolean
    */
@@ -9,23 +9,26 @@ class ValidacoesService {
     return nome.length >= 3 && str != nome;
   }
 
+  /**
+   * Método que valida se o e-mail está no padrão "string@string.com"
+   * @param {string} email 
+   * @returns boolean
+   */
+  static validaEmail(email) {
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+    return regex.test(email)
+  }
 
-    /**
-     * Método que valida se o e-mail está no padrão "string@string.com"
-     * @param {string} email 
-     * @returns boolean
-     */
-    static validaEmail(email) {
-        const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-        return regex.test(email)
-    }
-
+/**
+ * Metodo usa esta regex que valida para DD/MM/AAAA ou DD-MM-AAAA
+ * @param {string} data 
+ * @returns boolean
+ */
   static validaData(data) {
     const regexData =
       /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
     return regexData.test(data);
   }
-
 
   /**
    * Método que valida se o e-mail está no padrão "string@string.com"
@@ -37,22 +40,24 @@ class ValidacoesService {
     return regex.test(email);
   }
 
-
-        //const regex = /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/
-        //return  regex.test(telefone)
-    //}
-    
-    
-
   /**Método que valida se o telefone esta no padrão:
    * Exemplos válidos: +55 (11) 98888-8888 / 9999-9999 / 21 98888-8888 / 5511988888888
    * @param {string} telefone
    * @returns boolean
    */
   static validaTelefone(telefone) {
-    const regex =
-      /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/;
+    const regex = /^(?:(?:\+|00)?(55)\s?)?(?:(?:\(?[1-9][0-9]\)?)?\s?)?(?:((?:9\d|[2-9])\d{3})-?(\d{4}))$/
     return regex.test(telefone);
+  }
+  
+/**
+ * Metodo valida se o cnpj esta no padrao: 11.222.333/4444-55
+ * @param {string} cnpj 
+ * @returns boolean
+ */
+  static validaCnpj(cnpj){
+    const regexCnpj = /^(\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2})$/
+    return regexCnpj.test(cnpj)
   }
 
   /**

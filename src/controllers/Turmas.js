afe198b25cdf96caee4d90d405a03c8b34a680d1
Juliana <julianaha01@gmail.com>
2022-08-01 08:14:13 -0300

    fix: refatora rotas com validacoes da entidade turmas

--- src/controllers/Turmas.js
+++ src/controllers/Turmas.js
@@ -11,22 +11,18 @@ class Turmas {
 
     app.get("/turmas/:id", async (req, res) => {
       try {
-        const turmas = await DatabaseTurmasMetodos.listarTurmasPorId(
-          req.params.id
-        );
+        const turmas = await DatabaseTurmasMetodos.listarTurmasPorId(req.params.id);
         if (!turmas) {
           throw new Error("Turma não encontrada com esse Id");
         }
-        res.status(200).json(response);
+        res.status(200).json(turmas);
       } catch (error) {
         res.status(400).json(error.message);
       }
     });
 
     app.post("/turmas", async (req, res) => {
-      const validaTurma = ValidacoesTurmas.validaTurmas(
-        ...Object.values(req.body)
-      );
+      const validaTurma = ValidacoesTurmas.validaTurmas(...Object.values(req.body));
       try {
         if (validaTurma) {
           const turma = new TurmasModel(...Object.values(req.body));
@@ -40,29 +36,31 @@ class Turmas {
       }
     });
 
-    app.put("/turmas/:id", (req, res) => {
+    app.put("/turmas/:id", async (req, res) => {
       const validaTurma = ValidacoesTurmas.validaTurmas(
-        ...Object.values(req.body)
-      );
-      if (validaTurma) {
-        const turma = new TurmasModel(...Object.values(req.body));
-        const response = DatabaseTurmasMetodos.atualizaTurmaPorId(
-          req.params.id,
-          turma
-        );
-        res.status(201).json(response);
-      } else {
-        res.status(400).json({ Erro: "Erro" });
+        ...Object.values(req.body));
+      try {
+        if (validaTurma) {
+          const turma = new TurmasModel(...Object.values(req.body));
+          const response = await DatabaseTurmasMetodos.atualizaTurmaPorId(req.params.id, turma);
+          res.status(200).json(response);
+        } else {
+          throw new Error("Requisição inválida, revise o corpo da mesma")
+        }
+      } catch (error) {
+        res.status(400).json(error.message);
+
       }
     });
 
     app.delete("/turmas/:id", async (req, res) => {
-      const turma = await DatabaseTurmasMetodos.deletaTurmaPorId(req.params.id);
       try {
+        const turma = await DatabaseTurmasMetodos.listarTurmasPorId(req.params.id);
         if (!turma) {
           throw new Error("Turma não encontrada");
         }
-        res.status(200).json(turma);
+        const response = await DatabaseTurmasMetodos.deletaTurmaPorId(req.params.id)
+        res.status(200).json(response);
       } catch (error) {
         res.status(404).json({ Error: error.message });
       }

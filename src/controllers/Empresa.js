f85fa7506af5f947b5c6fcc43df0047af4ebb369
Juliana <julianaha01@gmail.com>
2022-08-01 07:32:16 -0300

    fix: refatora rotas da entidade empresa

--- src/controllers/Empresa.js
+++ src/controllers/Empresa.js
@@ -2,7 +2,6 @@ import EmpresaModel from "../models/EmpresaModel.js"
 import ValidacoesEmpresa from "../services/ValidacoesEmpresa.js"
 import DatabaseEmpresaMetodos from "../DAO/DatabaseEmpresaMetodos.js"
 
-DatabaseEmpresaMetodos.createTableEmpresa();
 class Empresa {
     static rotas(app) {
         app.get("/empresa", async (req, res) => {
@@ -32,7 +31,7 @@ class Empresa {
                 if (ValidaEmpresa) {
                     const empresa = new EmpresaModel(...Object.values(req.body))
                     const response = await DatabaseEmpresaMetodos.adicionaEmpresa(empresa)
-                    res.status(201).json(response)
+                    res.status(200).json(response)
                 } else {
                     throw new Error("Requisição incompleta, revise o corpo da mesma")
                 }
@@ -42,31 +41,32 @@ class Empresa {
         })
 
         app.put("/empresa/:id", async (req, res) => {
-            const validaEmpresa = ValidacoesEmpresa.validaEmpresa(...Object.values(req.body))
-
-            if (validaEmpresa) {
-                const empresa = new EmpresaModel(...Object.values(req.body))
-                const response = await DatabaseEmpresaMetodos.atualizaEmpresa(...Object.values(req.params.id), empresa)
-                res.status(201).json(response)
-            } else {
-                res.status(400).json()
+            const validaEmpresas = ValidacoesEmpresa.validaEmpresa(...Object.values(req.body))
+            try {
+                if (validaEmpresas) {
+                    const empresa = new EmpresaModel(...Object.values(req.body))
+                    const response = await DatabaseEmpresaMetodos.atualizaEmpresa(req.params.id, empresa)
+                    res.status(200).json(response)
+                } else {
+                    throw new Error("Requisição inválida, revise o corpo da mesma")
+                }
+            } catch (error) {
+                res.status(400).json(error.message)
             }
-
         })
 
         app.delete("/empresa/:id", async (req, res) => {
-            const empresa = await DatabaseEmpresaMetodos.excluirEmpresa(...Object.values(req.params.id))
             try {
+                const empresa = await DatabaseEmpresaMetodos.listarEmpresaPorId(req.params.id)
                 if (!empresa) {
                     throw new Error("Empresa não encontrada")
                 }
-                res.status(201).json(empresa)
+                const response = await DatabaseEmpresaMetodos.excluirEmpresa(req.params.id)
+                res.status(200).json(response)
             } catch (error) {
                 res.status(404).json({ Error: error.message })
             }
-
         })
-
     }
 }
 

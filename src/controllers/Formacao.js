aa5eefa860497c474703966527afae791395cae8
Juliana <julianaha01@gmail.com>
2022-08-02 21:42:09 -0300

    fix: refatora rotas em entidade formacao

--- src/controllers/Formacao.js
+++ src/controllers/Formacao.js
@@ -5,19 +5,24 @@ import ValidacoesFormacao from "../services/ValidacoesFormacao.js"
 class Formacao {
     static rotas(app) {
         app.get("/formacao", async (req, res) => {
-            const response = await DatabaseFormacaoMetodos.listarFormacao();
-            res.status(200).json(response);
+            try {
+                const response = await DatabaseFormacaoMetodos.listarFormacao();
+                res.status(200).json(response);
+            } catch (error) {
+                res.status(400).json(error.message)
+            }
+
         })
 
         app.get("/formacao/:id", async (req, res) => {
             try {
                 const formacao = await DatabaseFormacaoMetodos.listarFormacaoPorId(req.params.id);
                 if (!formacao) {
-                    throw new Error("Formação não encontrada com esse Id")
+                    throw new Error(`Formação com Id ${req.params.id} não existe`)
                 }
                 res.status(200).json(formacao);
             } catch (error) {
-                res.status(400).json(error.message)
+                res.status(400).json({ Error: error.message })
             }
         })
 
@@ -25,34 +30,33 @@ class Formacao {
             const validaFormacoes = ValidacoesFormacao.validaFormacao(...Object.values(req.body))
             try {
                 if (validaFormacoes) {
-                    debugger
                     const formacao = new FormacaoModel(...Object.values(req.body));
                     const response = await DatabaseFormacaoMetodos.inserirFormacao(formacao);
                     res.status(201).json(response);
                 } else {
-                    throw new Error("Requisição incompleta, revise o corpo da mesma")
+                    throw new Error("Requisição incorreta, revise o corpo da mesma")
                 }
             } catch (error) {
-                
-
                 res.status(400).json(error.message);
             }
 
         })
 
-        app.put("/formacao/:id", async(req, res) => {
-            const validaFormacoes = ValidacoesFormacao.validaFormacoes(...Object.values(req.body))
+        app.put("/formacao/:id", async (req, res) => {
+            const validaFormacoes = ValidacoesFormacao.validaFormacao(...Object.values(req.body))
             try {
-                if (validaFormacoes) {
+                const formacoes = await DatabaseFormacaoMetodos.listarFormacaoPorId(req.params.id);
+                if (!formacoes) {
+                    throw new Error(`Formação com Id ${req.params.id} não existe`)
+                } else if (validaFormacoes) {
                     const formacao = new FormacaoModel(...Object.values(req.body));
                     const response = await DatabaseFormacaoMetodos.atualizaFormacaoPorId(req.params.id, formacao);
                     res.status(201).json(response);
-
                 } else {
                     throw new Error("Requisição inválida, revise o corpo da mesma")
                 }
             } catch (error) {
-                res.status(400).json({ Erro: "Erro" });
+                res.status(400).json({ Error: error.message });
 
             }
 
@@ -60,9 +64,9 @@ class Formacao {
 
         app.delete("/formacao/:id", async (req, res) => {
             try {
-                const formacao = await DatabaseFormacaoMetodos.listaFormacaoPorId(req.params.id);
-                if(!formacao){
-                    throw new Error('Formação não encontrada')
+                const formacao = await DatabaseFormacaoMetodos.listarFormacaoPorId(req.params.id);
+                if (!formacao) {
+                    throw new Error(`Formação com Id ${req.params.id} não existe`);
                 }
                 const response = await DatabaseFormacaoMetodos.deletaFormacaoPorId(req.params.id)
                 res.status(200).json(response);

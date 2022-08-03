2fe6f55d0d093151ef72e8ea2e27d21976329ca9
Juliana <julianaha01@gmail.com>
2022-08-03 13:41:38 -0300

    fix: refatora validacao da entidade alunos

--- src/services/ValidacoesAluno.js
+++ src/services/ValidacoesAluno.js
@@ -1,19 +1,33 @@
 import ValidacoesService from "./ValidacoesServices.js";
 
-class ValidacoesAluno extends ValidacoesService{
-
-    static validaIdade(idade){
-        return idade >=16 && idade <=26
+class ValidacoesAluno extends ValidacoesService {
+    /**
+     * Metodo valida idade ente 16 e 26 anos;
+     * @param {number} idade 
+     * @returns boolean
+     */
+    static validaIdade(n) {
+        return !isNaN(parseFloat(n)) && n >= 16 && n <=26;
     }
 
-    static validaAluno(nome, email, telefone, idade){
+  /**
+   * Metodo valida todas as propriedades da entidade alunos;
+   * @param {string} nome 
+   * @param {string} email 
+   * @param {string} telefone 
+   * @param {number} idade 
+   * @param {string} turma 
+   * @param {string} curso 
+   * @returns boolean
+   */
+    static validaAluno(nome, email, telefone, idade, turma, curso) {
         return this.validaNome(nome) &&
-               this.validaEmail(email) &&
-               this.validaTelefone(telefone) && 
-               this.validaIdade(idade)
+            this.validaEmail(email) &&
+            this.validaTelefone(telefone) &&
+            this.validaIdade(idade) &&
+            this.validaClassificacao(turma) &&
+            this.validaNome(curso)
     }
-
 }
-
 
 export default ValidacoesAluno;

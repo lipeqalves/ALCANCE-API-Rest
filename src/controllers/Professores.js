007f7b1c62f7290c8d129c5bc822d2dd6b8a814c
Juliana <julianaha01@gmail.com>
2022-08-09 23:42:32 -0300

    feat: finaliza rotas professores

--- src/controllers/Professores.js
+++ src/controllers/Professores.js
@@ -1,5 +1,5 @@
 import ProfessoresModel from "../models/ProfessoresModel.js";
-import ValidacoesService from "../services/ValidacoesServices.js";
+import ValidacoesProfessores from "../services/ValidacoesProfessores.js";
 import DatabaseProfessoresMetodos from "../DAO/DatabaseProfessoresMetodos.js";
 
 class Professores {
@@ -9,28 +9,66 @@ class Professores {
         const response = await DatabaseProfessoresMetodos.listarProfessores();
         res.status(200).json(response);
       } catch (e) {
-        res.status(400).json({error: true, msg: e.message})
+        res.status(400).json({ error: true, msg: e.message })
       }
     });
-    app.get("/professores/:id", async(req, res)=>{
+    app.get("/professores/:id", async (req, res) => {
       try {
         const professor = await DatabaseProfessoresMetodos.listarProfessoresPorId(req.params.id);
-        if(!professor){
-          throw new Error(`Professor não encontrado com esse ID:${req.params.id}. Tente novamente.`)
+        if (!professor) {
+          throw new Error(`Professor não encontrado com esse ID:${req.params.id}`)
         }
         res.status(200).json(professor);
       } catch (e) {
-        res.status(400).json({Error: true, msg: e.message})
+        res.status(400).json({ Error: true, msg: e.message })
       }
     });
 
     app.post("/professores", async (req, res) => {
-      const profModel = new ProfessoresModel(...Object.values(req.body));
-      const response = await DatabaseProfessoresMetodos.adicionaProfessor(
-        profModel
-      );
-      res.status(201).json(response);
+      const validaProf = ValidacoesProfessores.validaProfessor(...Object.values(req.body))
+      try {
+        if (validaProf) {
+          const prof = new ProfessoresModel(...Object.values(req.body))
+          const response = await DatabaseProfessoresMetodos.adicionaProfessor(prof)
+          res.status(201).json(response)
+        } else {
+          throw new Error('Requisição incompleta, revise o corpo da mesma')
+        }
+      } catch (error) {
+        res.status(400).json(error.message)
+      }
     });
+
+    app.put("/professores/:id", async (req, res) => {
+      const validaProf = ValidacoesProfessores.validaProfessor(...Object.values(req.body))
+      try {
+        const prof = await DatabaseProfessoresMetodos.listarProfessoresPorId(req.params.id)
+        if (!prof) {
+          throw new Error(`Professor com Id ${req.params.id} não existe`)
+        } else if (validaProf) {
+          const professores = new ProfessoresModel(...Object.values(req.body))
+          const response = await DatabaseProfessoresMetodos.atualizaProfessoresPorId(req.params.id, professores)
+          res.status(201).json(response)
+        } else {
+          throw new Error("Requisição inválida, revise o corpo da mesma")
+        }
+      } catch (error) {
+        res.status(400).json({ Error: error.message })
+      }
+    })
+
+    app.delete("/professores/:id", async (req, res) => {
+      try {
+        const professores = await DatabaseProfessoresMetodos.listarProfessoresPorId(req.params.id)
+        if (!professores) {
+          throw new Error(`Professor com Id ${req.params.id} não encontrado`)
+        }
+        const response = await DatabaseProfessoresMetodos.deletaProfessoresPorId(req.params.id)
+        res.status(200).json(response)
+      } catch (error) {
+        res.status(404).json({ Error: error.message })
+      }
+    })
   }
 }
 
